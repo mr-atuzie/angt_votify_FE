f1f13332e127b4838b1eb0416328c85177fd4f85
rex atuzie <rexatuzie@gmail.com>
2025-01-28 16:40:12 +0100

    ----iphone bug 2--

--- src/components/Private.jsx
+++ src/components/Private.jsx
@@ -1,37 +1,37 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import {
-  fetchLoginStatus,
+  // fetchLoginStatus,
   selectIsLoggedIn,
 } from "../redux/features/auth/authSlice";
-import Loader from "./Loader";
+// import Loader from "./Loader";
 
 const Private = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  const [loading, setLoading] = useState(true); // Track loading state
+  // const [loading, setLoading] = useState(true); // Track loading state
 
-  useEffect(() => {
-    console.log("Private page....");
+  // useEffect(() => {
+  //   console.log("Private page....");
 
-    const checkLoginStatus = async () => {
-      try {
-        await dispatch(fetchLoginStatus()).unwrap(); // Dispatch and resolve async thunk
-      } catch (error) {
-        console.error("Failed to fetch login status:", error);
-      } finally {
-        setLoading(false); // Stop loading regardless of outcome
-      }
-    };
+  //   const checkLoginStatus = async () => {
+  //     try {
+  //       await dispatch(fetchLoginStatus()).unwrap(); // Dispatch and resolve async thunk
+  //     } catch (error) {
+  //       console.error("Failed to fetch login status:", error);
+  //     } finally {
+  //       setLoading(false); // Stop loading regardless of outcome
+  //     }
+  //   };
 
-    checkLoginStatus();
-  }, [dispatch]);
+  //   checkLoginStatus();
+  // }, [dispatch]);
 
   // Show a loading indicator while checking login status
-  if (loading) {
-    return <Loader />;
-  }
+  // if (loading) {
+  //   return <Loader />;
+  // }
 
   // Navigate to login if not logged in
   return isLoggedIn ? <Outlet /> : <Navigate to="/login" replace />;
