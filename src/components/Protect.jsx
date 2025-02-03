6eb024e1ad56637d6df958ac7c859114f0f9d3d7
rex atuzie <rexatuzie@gmail.com>
2025-02-03 15:33:17 +0100

    ---

--- src/components/Protect.jsx
+++ src/components/Protect.jsx
@@ -1,8 +1,7 @@
 import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from "../redux/features/auth/authSlice";
 
 const ShowOnLogin = ({ children }) => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isLoggedIn = sessionStorage.getItem("token");
 
   if (isLoggedIn) {
     return children;
